bf64a80be0474faa17e153927ff7eeebd3ca0101
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-15 23:34:17 +0200

    Hangman 2-Stage

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,8 +1,17 @@
 package hangman;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class hangman {
     public static void main(String[] args) {
-        System.out.println("HANGMAN\n" +
-                "The game will be available soon.");
+        System.out.println("HANGMAN\nGuess the word:");
+        Scanner input = new Scanner(System.in);
+        if(Objects.equals(input.next(), "python")){
+            System.out.println("You win!");
+        }
+        else{
+            System.out.println("You lose!");
+        }
     }
 }
