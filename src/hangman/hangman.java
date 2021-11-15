af1a46cec55f7f783f648f0140100f1955f46b44
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-15 22:53:56 +0200

    Hangman 1-Stage

---
+++ src/hangman/hangman.java
@@ -0,0 +1,8 @@
+package hangman;
+
+public class hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN\n" +
+                "The game will be available soon.");
+    }
+}
