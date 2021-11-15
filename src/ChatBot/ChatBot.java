b189c965bb184ce6bbc2850a75f12c8a3b03cad3
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-15 21:26:24 +0200

    ChatBot 2-Stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,13 @@
 package ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Alex.\n" +
-                "I was created in 2021.");
+                "I was created in 2021.\n" +
+                "Please, remind me your name.");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What a great name you have, " + input.next() + "!");
     }
 }
