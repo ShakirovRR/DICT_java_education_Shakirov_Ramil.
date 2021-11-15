8860f468dc1095175d3ddbbd674d4211365cbc30
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-15 21:33:11 +0200

    ChatBot 3-stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -8,6 +8,9 @@ public class ChatBot {
                 "I was created in 2021.\n" +
                 "Please, remind me your name.");
         Scanner input = new Scanner(System.in);
-        System.out.println("What a great name you have, " + input.next() + "!");
+        System.out.println("What a great name you have, " + input.next() + "!\n" +
+                "Let me guess your age.\n" + "Enter remainders of dividing your age by 3, 5 and 7.");
+        int age = (input.nextInt() * 70 + input.nextInt() * 21 + input.nextInt() * 15) % 105;
+        System.out.println("Your age is " + age + "; a good time to start programming!");
     }
 }
