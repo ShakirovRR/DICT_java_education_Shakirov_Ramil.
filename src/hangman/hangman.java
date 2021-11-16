d23c01ba35e35ad5b64d294e8cab47b37c890a74
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-16 22:35:17 +0200

    Hangman 5-Stage

--- src/hangman/hangman.java
+++ src/hangman/hangman.java
@@ -1,23 +1,64 @@
 package hangman;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
 
+class Hangman{
+    private final ArrayList<String> words = new ArrayList<String>();
+    private ArrayList<String> lettersInWord = new ArrayList<String>();
+    private ArrayList<String> word;
+
+    private int HEALTH = 8;
+
+
+    public void createRandomWord(){
+        Random rand = new Random();
+        String[] words = new String[]{"java","kotlin","python","javascript"};
+        String randWord = words[rand.nextInt(4)];
+        word = new ArrayList<String>();
+        for (int i = 0;i<randWord.length();i++){
+            word.add(Character.toString(randWord.charAt(i)));
+        }
+        lettersInWord = new ArrayList<String>();
+    }
+    public void printSecretWord(){
+        for (String s : word) {
+            if (lettersInWord.contains(s)) {
+                System.out.print(s);
+            } else {
+                System.out.print("-");
+            }
+
+        }
+        System.out.println();
+    }
+    public void addLetter(String letter){
+        if(word.contains(letter)){
+            lettersInWord.add(letter);
+        }
+        else {
+            this.HEALTH-=1;
+        }
+    }
+
+
+
+}
+
 public class hangman {
     public static void main(String[] args) {
-        String[] words = new String[]{"java","kotlin","python","javascript"};
-        String[] secretWords = new String[]{"ja--","ko----","py----","ja--------"};
-        Random rand = new Random();
-        int indexWord = rand.nextInt(4);
-        String word = words[indexWord];
-        System.out.println("HANGMAN\nGuess the word " + secretWords[indexWord] + ":");
+        Hangman player = new Hangman();
         Scanner input = new Scanner(System.in);
-        if(Objects.equals(input.next(), word)){
-            System.out.println("You survived!");
-        }
-        else{
-            System.out.println("You lose!");
+        System.out.println("HANGMAN:");
+        player.createRandomWord();
+        while (true) {
+            player.printSecretWord();
+            String letter = input.next();
+            player.addLetter(letter);
+
+
         }
     }
 }
