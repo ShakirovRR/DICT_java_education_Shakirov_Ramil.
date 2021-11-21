0e0bc9629d2aa398593c61b327915418f2067123
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-21 16:40:32 +0200

    Tictactoe 2-Stage

--- src/tictactoe/tictactoe.java
+++ src/tictactoe/tictactoe.java
@@ -1,9 +1,15 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class tictactoe {
     public static void main(String[] args) {
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O");
+        Scanner input = new Scanner(System.in);
+        String field = input.next();
+        System.out.println("---------\n" +
+                "| " + field.charAt(0) + " " + field.charAt(1) + " " + field.charAt(2) + " |\n" +
+                "| " + field.charAt(3) + " " + field.charAt(4) + " " + field.charAt(5) + " |\n" +
+                "| " + field.charAt(6) + " " + field.charAt(7) + " " + field.charAt(8) + " |\n" +
+                "---------");
     }
 }
