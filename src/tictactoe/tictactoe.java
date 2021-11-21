a1d164525d85776a212251fe7a3709d1cf12ca09
Shakirov <r.r.shakirov@student.khai.edu>
2021-11-21 16:36:28 +0200

    Tictactoe 1-Stage

---
+++ src/tictactoe/tictactoe.java
@@ -0,0 +1,9 @@
+package tictactoe;
+
+public class tictactoe {
+    public static void main(String[] args) {
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O");
+    }
+}
